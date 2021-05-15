ba6278884b71b1a6ba3ad86bd26abb76bb323905
Igor Martayan <igor@martayan.org>
2021-05-15 13:46:38 +0200

    Add dancing links implementation

---
+++ dlx.cpp
@@ -0,0 +1,119 @@
+#include "dlx.hpp"
+
+Cell *Cell::get_next() const { return next; }
+Line *Cell::get_line() const { return line; }
+Column *Cell::get_col() const { return col; }
+
+void Cell::remove() const {
+  previous->next = next;
+  next->previous = previous;
+}
+
+void Cell::restore() {
+  previous->next = this;
+  next->previous = this;
+}
+
+void Cell::append(Cell *other) {
+  other->previous = this;
+  other->next = next;
+  next->previous = other;
+  next = other;
+}
+
+Line::Line(std::vector<Column *> &selected_cols) {
+  for (auto col : selected_cols) {
+    auto cell = new Cell(this, col);
+    cells.push_back(cell);
+    col->head->append(cell);
+    col->size++;
+  }
+}
+
+Line::~Line() {
+  for (auto cell : cells)
+    delete cell;
+}
+
+bool Line::is_selected() const { return state == 1; }
+
+std::vector<std::vector<Line *>> Line::select() {
+  std::vector<std::vector<Line *>> stacks;
+  if (state == 0) {
+    state = 1;
+    for (auto cell : cells)
+      stacks.push_back(cell->get_col()->remove());
+  }
+  return stacks;
+}
+
+void Line::unselect(std::vector<std::vector<Line *>> &stacks) {
+  if (state == 1) {
+    state = 0;
+    for (int i = stacks.size() - 1; i >= 0; i--)
+      cells[i]->get_col()->restore(stacks[i]);
+  }
+}
+
+void Line::remove() {
+  if (state == 0) {
+    state = -1;
+    for (auto cell : cells) {
+      cell->remove();
+      auto col = cell->get_col();
+      col->size--;
+      if (col->has_heap())
+        col->heap->decrease(col, col->size);
+    }
+  }
+}
+
+void Line::restore() {
+  if (state == -1) {
+    state = 0;
+    for (int i = cells.size() - 1; i >= 0; i--) {
+      cells[i]->restore();
+      auto col = cells[i]->get_col();
+      col->size++;
+      if (col->has_heap())
+        col->heap->increase(col, col->size);
+    }
+  }
+}
+
+Column::Column() : Column(nullptr) {}
+
+Column::Column(Heap<Column *, int> *h)
+    : head(new Cell(nullptr, this)), heap(h) {}
+
+Column::~Column() { delete head; }
+
+int Column::get_size() const { return size; }
+Cell *Column::get_head() const { return head; }
+bool Column::has_heap() const { return heap != nullptr; }
+
+std::vector<Line *> Column::remove() {
+  if (has_heap())
+    heap->remove(this);
+  std::vector<Line *> stack;
+  Cell *cell = head->get_next();
+  while (cell != head) {
+    auto line = cell->get_line();
+    if (line->state == 0) {
+      line->remove();
+      stack.push_back(line);
+    }
+    cell = cell->get_next();
+  }
+  return stack;
+}
+
+void Column::restore(std::vector<Line *> &stack) {
+  while (!stack.empty()) {
+    auto line = stack.back();
+    line->restore();
+    stack.pop_back();
+  }
+  if (has_heap())
+    heap->restore(this);
+}
---
+++ dlx.hpp
@@ -0,0 +1,66 @@
+#ifndef DLX_HPP
+#define DLX_HPP
+
+#include "heap.hpp"
+#include <vector>
+
+class Line;
+class Column;
+
+class Cell {
+private:
+  Cell *previous;
+  Cell *next;
+  Line *const line;
+  Column *const col;
+
+public:
+  Cell(Line *l, Column *c) : previous(this), next(this), line(l), col(c) {}
+
+  Cell *get_next() const;
+  Line *get_line() const;
+  Column *get_col() const;
+
+  void remove() const;
+  void restore();
+  void append(Cell *other);
+};
+
+class Line {
+private:
+  friend Column;
+  int state = 0;
+  std::vector<Cell *> cells;
+
+public:
+  Line(std::vector<Column *> &selected_cols);
+  ~Line();
+
+  bool is_selected() const;
+  std::vector<std::vector<Line *>> select();
+  void unselect(std::vector<std::vector<Line *>> &stacks);
+  void remove();
+  void restore();
+};
+
+class Column {
+private:
+  friend Line;
+  int size = 0;
+  Cell *const head;
+  Heap<Column *, int> *const heap;
+
+public:
+  Column();
+  Column(Heap<Column *, int> *h);
+  ~Column();
+
+  int get_size() const;
+  Cell *get_head() const;
+  bool has_heap() const;
+
+  std::vector<Line *> remove();
+  void restore(std::vector<Line *> &stack);
+};
+
+#endif // DLX_HPP
